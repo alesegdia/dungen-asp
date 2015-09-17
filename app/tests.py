e46951f86a67cca5872257b0d43bdb74f53d3429
Alejandro Seguí <alesegdia@gmail.com>
2015-09-17 03:06:04 +0200

    updating tests with new folder structure; testing clingo process spawn

--- app/tests.py
+++ app/tests.py
@@ -1,6 +1,6 @@
 
-from tilemap import Tilemap
-from aspgen import RoomModelASP, asp_room_entry
+from maputil.tilemap import Tilemap
+from asp.generator import RoomModelASP, asp_room_entry
 
 tm = Tilemap(10,10)
 tm.tilemap[0][2] = 1
@@ -10,3 +10,7 @@ tm.tilemap[4][6] = 1
 tm.tilemap[5][6] = 1
 
 print(asp_room_entry(tm, "qweqwe", 10))
+
+from asp.clingo import clingo_spawn
+
+print clingo_spawn('fact.')
